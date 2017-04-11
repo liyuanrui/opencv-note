833600a00fd2baf347b83f019129dd65d5a0c2bd
lr <i@lr.cool>
2017-04-11 14:09:57 +0800

    绘图ok

--- part2.py
+++ part2.py
@@ -3,12 +3,9 @@
 import numpy as np
 import cv2
 
-#创建一个长宽512的黑色图片
 img=np.zeros((512,512,3),np.uint8)
-
-#画一条坐标(0,0)到(511,511)的蓝色直线，厚度为5
-img=cv2.line(img,(0,0),(511,511),(255,0,0),5)
-
+font=cv2.FONT_HERSHEY_SIMPLEX
+img=cv2.putText(img,'OpenCV',(10,500),font,4,(255,255,255),5,cv2.LINE_AA)
 
 cv2.imshow('image',img)
 if cv2.waitKey(0) & 0xFF == ord('q'):
