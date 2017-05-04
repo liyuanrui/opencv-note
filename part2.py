de5fc673c5494eac9d2e7619cf190518ac1f060f
lr <i@lr.cool>
2017-05-04 21:10:52 +0700

    终于到边缘检测啦

--- part2.py
+++ part2.py
@@ -7,22 +7,13 @@ from matplotlib import pyplot as plt
 
 os.chdir('C:/Users/LR/Desktop')
 
-BLUE=[255,0,0]
 
-img1=cv2.imread('143.jpg')
 
-#重复，空白
-replicate=cv2.copyMakeBorder(img1,30,30,30,30,cv2.BORDER_REPLICATE)
+img=cv2.imread('123.jpg',0)
 
-#映射，镜面
-reflect=cv2.copyMakeBorder(img1,30,30,30,30,cv2.BORDER_REFLECT)
-reflect101=cv2.copyMakeBorder(img1,30,30,30,30,cv2.BORDER_REFLECT_101)
+kernel=np.ones((5,5),np.uint8)
+erosion= cv2.morphologyEx(img,cv2.MORPH_OPEN,kernel)
 
-#包围
-wrap=cv2.copyMakeBorder(img1,30,30,30,30,cv2.BORDER_WRAP)
-
-constant=cv2.copyMakeBorder(img1,30,30,30,30,cv2.BORDER_CONSTANT,value=BLUE)
-
-cv2.imshow('image',constant)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imshow('image',erosion)
+if cv2.waitKey(0) & 0xFF ==ord('q'):
+    cv2.destroyAllWindows()
