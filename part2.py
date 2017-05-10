08f55a4cb13bdb6b842e65550de174e2e7459a26
lr <i@lr.cool>
2017-05-10 11:36:12 +0700

    commit

--- part2.py
+++ part2.py
@@ -8,12 +8,15 @@ from matplotlib import pyplot as plt
 os.chdir('C:/Users/LR/Desktop')
 
 
+#加载灰度图像
+img=cv2.imread('1234.png',0)
 
-img=cv2.imread('123.jpg',0)
+#二值化，注意这里最后一个参数用0也可以
+ret,thresh=cv2.threshold(img,127,255,0)
 
-kernel=np.ones((5,5),np.uint8)
-erosion= cv2.morphologyEx(img,cv2.MORPH_OPEN,kernel)
 
-cv2.imshow('image',erosion)
-if cv2.waitKey(0) & 0xFF ==ord('q'):
-    cv2.destroyAllWindows()
+contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+#cv2.imshow('image',contours)
+#if cv2.waitKey(0) & 0xFF ==ord('q'):
+#    cv2.destroyAllWindows()
