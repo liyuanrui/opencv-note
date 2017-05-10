b1af9affdcafcc717e89ddfabba586956e95dab5
lr <i@lr.cool>
2017-05-10 17:43:51 +0700

    commit

--- part2.py
+++ part2.py
@@ -15,8 +15,20 @@ img=cv2.imread('1234.png',0)
 ret,thresh=cv2.threshold(img,127,255,0)
 
 
-contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours,hierarchy = cv2.findContours(thresh,2,1)
+cnt = contours[0]
 
-#cv2.imshow('image',contours)
-#if cv2.waitKey(0) & 0xFF ==ord('q'):
-#    cv2.destroyAllWindows()
+hull = cv2.convexHull(cnt,returnPoints = False)
+defects = cv2.convexityDefects(cnt,hull)
+
+for i in range(defects.shape[0]):
+    s,e,f,d = defects[i,0]
+    start = tuple(cnt[s][0])
+    end = tuple(cnt[e][0])
+    far = tuple(cnt[f][0])
+    cv2.line(img,start,end,[0,255,0],2)
+    cv2.circle(img,far,5,[0,0,255],-1)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
