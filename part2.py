01dd4c1495421da3f87d59a3b72259466a4cdc19
lr <i@lr.cool>
2017-05-11 16:16:56 +0700

    commit

--- part2.py
+++ part2.py
@@ -9,26 +9,14 @@ os.chdir('C:/Users/LR/Desktop')
 
 
 #加载灰度图像
-img=cv2.imread('1234.png',0)
-
-#二值化，注意这里最后一个参数用0也可以
-ret,thresh=cv2.threshold(img,127,255,0)
 
 
-contours,hierarchy = cv2.findContours(thresh,2,1)
-cnt = contours[0]
+img=cv2.imread('333.jpg')
 
-hull = cv2.convexHull(cnt,returnPoints = False)
-defects = cv2.convexityDefects(cnt,hull)
+hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-for i in range(defects.shape[0]):
-    s,e,f,d = defects[i,0]
-    start = tuple(cnt[s][0])
-    end = tuple(cnt[e][0])
-    far = tuple(cnt[f][0])
-    cv2.line(img,start,end,[0,255,0],2)
-    cv2.circle(img,far,5,[0,0,255],-1)
+hist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imshow('image',hist)
+if cv2.waitKey(0) & 0xFF ==ord('q'):
+    cv2.destroyAllWindows()
